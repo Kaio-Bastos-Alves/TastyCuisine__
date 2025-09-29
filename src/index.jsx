127d6fdb80fcd8e371e38ddc92f7f8498b575dd0
KAIO-BASTOS-ALVES <rm95196@estudante.fieb.edu.br>
2025-09-29 08:33:06 -0300

    TastyCuisine

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './App';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
