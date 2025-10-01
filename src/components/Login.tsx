4606cc7f47a7872ae7ca473e0c46d4556facab6a
KAIO-BASTOS-ALVES <rm95196@estudante.fieb.edu.br>
2025-10-01 11:05:46 -0300

    assa

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,11 +3,12 @@ import './Login.css'
 function Login( ){
     return (
         <div className='page'>
-            <h1>AAAA</h1>
+            <h1>Login</h1>
             <div className="inputs">
-                <input className='input' type="text" placeholder="Seu nome..."/>
-                <input className='input' type="text" placeholder="Sua senha..."/>
+                <input className='Nome' type="text" placeholder="Seu nome..."/>
+                <input className='Senha' type="text" placeholder="Sua senha..."/>
             </div>
+            <button className='botao'>Entrar</button>
         </div>
     
 )
